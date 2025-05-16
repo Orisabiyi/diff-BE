0c2bb5dd1f3ac296548142608526dabccbf3ffe2
orisabiyi <orisabiyidavid@gmail.com>
2025-05-16 11:18:12 +0100

    fix: update .gitignore to include pnpm-lock.json and populate.controller.ts

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,12 @@
 import type { FastifyInstance, FastifyPluginOptions } from "fastify";
 import { createUser, getUser } from "../controllers/user.controller";
 import { getUserSchema, userSchema } from "../models/user.schema";
+// import { populateDB } from "../controllers/populate.controller";
 
 async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
   fastify.post('/create-user', { schema: userSchema }, createUser)
   fastify.post('/login', { schema: getUserSchema }, getUser)
+  // fastify.post('/populate', populateDB)
 }
 
 export default routes

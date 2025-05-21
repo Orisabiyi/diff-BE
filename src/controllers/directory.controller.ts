d9b406f96752594062a5dcc5b2a1fbfb3621d0e0
orisabiyi <orisabiyidavid@gmail.com>
2025-05-21 08:04:11 +0100

    feat: update getDirectories function to handle query parameters for category filtering

--- src/controllers/directory.controller.ts
+++ src/controllers/directory.controller.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 
-export async function getDirectories(this: FastifyInstance, request: FastifyRequest<{ Params: { action: string } }>, reply: FastifyReply) {
+export async function getDirectories(this: FastifyInstance, request: FastifyRequest<{ Params: { action: string }, Querystring: { tag?: string } }>, reply: FastifyReply) {
   const { action } = request.params;
+  const { tag } = request.query;
   const collection = this.mongo.db?.collection("directory-data");
 
   if (!collection) {
@@ -22,6 +23,24 @@ export async function getDirectories(this: FastifyInstance, request: FastifyRequ
         tags: 1
       }
     }).limit(100).toArray();
+    return reply.status(200).send({
+      message: "Directories retrieved successfully",
+      data: directories,
+    });
+  }
+
+  if (action === 'category' && tag) {
+    const directories = await collection.find({ tags: { $in: [tag] } }, {
+      projection: {
+        _id: 0,
+        name: 1,
+        type: 1,
+        description: 1,
+        website: 1,
+        tags: 1
+      }
+    }).limit(100).toArray();
+
     return reply.status(200).send({
       message: "Directories retrieved successfully",
       data: directories,

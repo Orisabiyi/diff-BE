cca72a380b1b1307a80cb874cbfa90b6692dda5f
orisabiyi <orisabiyidavid@gmail.com>
2025-05-30 13:02:42 +0100

    feat: add chat controller and types for chat creation
    - Implemented  function in  to handle chat recommendations.
    - Added validation for required fields: userId, chatId, and text.
    - Created a new interface  in  to define the structure of chat creation requests.
    - Added placeholder functions for starting a chat and retrieving user chats.
    - Created an empty  file for future authentication middleware implementation.

---
+++ src/controllers/chat.controller.ts
@@ -0,0 +1,30 @@
+import { FastifyInstance } from "fastify/types/instance";
+import { FastifyReply } from "fastify/types/reply";
+import { FastifyRequest } from "fastify/types/request";
+import { CreateChat } from "../types/controller";
+
+
+
+export async function recommendFund(this: FastifyInstance, request: FastifyRequest<{ Body: CreateChat }>, reply: FastifyReply) {
+  const { userId, chatId, text } = request.body;
+
+  if (!userId || !chatId || !text) return reply.status(400).send({
+    message: "userId, chatId and text are required"
+  })
+
+  // check if user exists
+  const userCollection = this.mongo.db?.collection("users");
+  const user = await userCollection?.findOne({ id: userId });
+
+  if (!user) return reply.status(400).send({
+    message: "userId is not valid"
+  })
+
+  const collection = this.mongo.db?.collection("recommedations");
+}
+
+export async function startChat() { }
+
+export async function getUserAllChat() { }
+
+export async function getChatById() { }
---
+++ src/types/controller.d.ts
@@ -0,0 +1,5 @@
+export interface CreateChat {
+  userId: string;
+  chatId: string;
+  text: string;
+}
